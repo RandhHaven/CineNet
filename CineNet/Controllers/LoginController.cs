b06505866325f703a29b3143e88a7035508df042
Randhall Rangel <randhall.rd@gmail.com>
2018-11-19 18:39:34 -0300

    Creacion del Proyecto.

---
+++ CineNet/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+
+namespace CineNet.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult ViewLogin()
+        {
+            return View();
+        }
+
+        // GET: Login
+        public ActionResult ViewInicio()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ingresar(string user, string password)
+        {
+            //Crear todo el esquema de ingreso.
+            /*Usuario usuario = CoreUsuario.ValidarYObtenerUsuario(user, password);
+            if (object.Equals(usuario, null))
+            {
+            }
+            else
+            {
+            }*/
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(string userName, string email, string password)
+        {
+            //Usuario usuario = CoreUsuario.RegistrarNuevoUsuario(userName, email, password);
+            return View();
+        }
+    }
+}
