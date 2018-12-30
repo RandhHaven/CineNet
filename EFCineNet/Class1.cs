734d27bace391aa6b7f5b524f5d055826703198b
Randhall Rangel <rrangel@est.frba.utn.edu.ar>
2018-12-29 21:08:20 -0300

    Avance de Proyecto Cine

---
+++ EFCineNet/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCineNet
+{
+    public class Class1
+    {
+    }
+}
