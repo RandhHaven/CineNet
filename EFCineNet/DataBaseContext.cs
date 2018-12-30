5ea30a5ce4c23a5cb014f4d73ddd0ca89f7ef808
Randhall Rangel <rrangel@est.frba.utn.edu.ar>
2018-12-29 22:26:37 -0300

    Creacion de DB por EF

--- EFCineNet/DataBaseContext.cs
+++ EFCineNet/DataBaseContext.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EFCineNet
 {
-    public class Class1
+    public class DataBaseContext : DbContext
     {
     }
 }
